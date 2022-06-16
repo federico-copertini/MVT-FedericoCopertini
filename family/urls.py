00e4b9820830d2eadc2f26d30f7330eb758b21ed
federico-copertini <fedecoper@hotmail.com>
2022-06-16 17:24:30 -0300

    segundo intento

--- family/urls.py
+++ family/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('familyapp', include("familyapp.urls")),
+    path('', include("familyapp.urls")),
 ]
